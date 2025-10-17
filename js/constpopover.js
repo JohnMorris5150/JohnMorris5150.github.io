089f39569518672744774ac94ef0bc3a41b8cf4e
Morris5150 <john.morris@mymail.champlain.edu>
2025-10-17 01:40:57 -0500

    Implement form validation functions
    Added form validation functions for name, email, and phone.

--- js/constpopover.js
+++ js/constpopover.js
@@ -1,3 +1,32 @@
 $(function () {
   $('[data-toggle="popover"]').popover()
 })
+
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+
+function validateForm() {
+  let x = document.forms["contactForm"]["fname"].value;
+  if (x == "") {
+    alert("Please input your name");
+     return false;
+  }
+}
+
+function validateEmail() {
+  let x = document.forms["contactForm2"]["femail"].value;
+  if (x == "") {
+    alert("Please input your email");
+    return false;
+    }
+}
+
+function validatePhone() {
+  let x = document.forms["contactForm3"]["fphone"].value;
+  if (x == "") {
+    alert("Please input a valid number");
+    return false;
+    }
+}
+
